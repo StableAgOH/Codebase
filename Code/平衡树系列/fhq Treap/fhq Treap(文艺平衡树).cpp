a8120406d61467d68708d02facc2ed0409cb430b
AgOH <1305707442@qq.com>
2019-07-21 20:00:18 +0800

    Create fhq Treap(文艺平衡树).cpp

---
+++ Code/平衡树系列/fhq Treap/fhq Treap(文艺平衡树).cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <cctype>
+namespace FastIO
+{
+char buf[1 << 21], buf2[1 << 21], a[20], *p1 = buf, *p2 = buf, hh = '\n';
+int p, p3 = -1;
+void read() {}
+void print() {}
+inline int getc()
+{
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1 << 21, stdin), p1 == p2) ? EOF : *p1++;
+}
+inline void flush()
+{
+    fwrite(buf2, 1, p3 + 1, stdout), p3 = -1;
+}
+template <typename T, typename... T2>
+inline void read(T &x, T2 &... oth)
+{
+    int f = 0;
+    x = 0;
+    char ch = getc();
+    while (!isdigit(ch))
+    {
+        if (ch == '-')
+            f = 1;
+        ch = getc();
+    }
+    while (isdigit(ch))
+    {
+        x = x * 10 + ch - 48;
+        ch = getc();
+    }
+    x = f ? -x : x;
+    read(oth...);
+}
+template <typename T, typename... T2>
+inline void print(T x, T2... oth)
+{
+    if (p3 > 1 << 20)
+        flush();
+    if (x < 0)
+        buf2[++p3] = 45, x = -x;
+    do
+    {
+        a[++p] = x % 10 + 48;
+    } while (x /= 10);
+    do
+    {
+        buf2[++p3] = a[p];
+    } while (--p);
+    buf2[++p3] = hh;
+    print(oth...);
+}
+} // namespace FastIO
+#define read FastIO::read
+#define print FastIO::print
+//======================================
+const int maxn = 1e5+5;
+struct Node
+{
+    int l,r;
+    int val,key;
+    int size;
+    bool reverse;
+}fhq[maxn];
+int cnt,root;
+#include <random>
+std::mt19937 rnd(233);
+inline int newnode(int val)
+{
+    fhq[++cnt].val=val;
+    fhq[cnt].key=rnd();
+    fhq[cnt].size=1;
+    return cnt;
+}
+inline void update(int now)
+{
+    fhq[now].size=fhq[fhq[now].l].size+fhq[fhq[now].r].size+1;
+}
+inline void pushdown(int now)
+{
+    std::swap(fhq[now].l,fhq[now].r);
+    fhq[fhq[now].l].reverse^=1;
+    fhq[fhq[now].r].reverse^=1;
+    fhq[now].reverse=false;
+}
+void split(int now,int siz,int &x,int &y)
+{
+    if(!now) x=y=0;
+    else 
+    {
+        if(fhq[now].reverse) pushdown(now);
+        if(fhq[fhq[now].l].size<siz)
+        {
+            x=now;
+            split(fhq[now].r,siz-fhq[fhq[now].l].size-1,fhq[now].r,y);
+        }
+        else 
+        {
+            y=now;
+            split(fhq[now].l,siz,x,fhq[now].l);
+        }
+        update(now);
+    }
+}
+int merge(int x,int y)
+{
+    if(!x||!y) return x+y;
+    if(fhq[x].key<fhq[y].key)
+    {
+        if(fhq[x].reverse) pushdown(x);
+        fhq[x].r=merge(fhq[x].r,y);
+        update(x);
+        return x;
+    }
+    else 
+    {
+        if(fhq[y].reverse) pushdown(y);
+        fhq[y].l=merge(x,fhq[y].l);
+        update(y);
+        return y;
+    }
+}
+void reverse(int l,int r)
+{
+    int x,y,z;
+    split(root,l-1,x,y);
+    split(y,r-l+1,y,z);
+    fhq[y].reverse^=1;
+    root=merge(merge(x,y),z);
+}
+void ldr(int now)
+{
+    if(!now) return;
+    if(fhq[now].reverse) pushdown(now);
+    ldr(fhq[now].l);
+    print(fhq[now].val);
+    ldr(fhq[now].r);
+}
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+#endif
+    clock_t c1 = clock();
+    //======================================
+    FastIO::hh = ' ';
+    int n,m;
+    read(n,m);
+    for(int i=1;i<=n;i++)
+        root=merge(root,newnode(i));
+    while(m--)
+    {
+        int l,r;
+        read(l,r);
+        reverse(l,r);
+    }
+    ldr(root);
+    //======================================
+    FastIO::flush();
+    std::cerr << "Time:" << clock() - c1 << "ms" << std::endl;
+    return 0;
+}
+/*
+ * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+ * │Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│  ┌┐    ┌┐    ┌┐
+ * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘  └┘    └┘    └┘
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
+ * │~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│ │Num│ / │ * │ - │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
+ * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│ │ 7 │ 8 │ 9 │   │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┤ + │
+ * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │               │ 4 │ 5 │ 6 │   │
+ * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
+ * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │     │ 1 │ 2 │ 3 │   │
+ * ├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤ E││
+ * │ Ctrl│ Win│ Alt│         Space         │ Alt│ Win│Menu│Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │←─┘│
+ * └─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
+ */
