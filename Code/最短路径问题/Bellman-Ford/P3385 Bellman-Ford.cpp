1449abdf4ba07f77d0bf703692f2cdbdd9c8be64
StableAgOH <1305707442@qq.com>
2019-11-10 19:57:04 +0800

    Create P3385 Bellman-Ford.cpp

---
+++ Code/最短路径问题/Bellman-Ford/P3385 Bellman-Ford.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <cctype>
+namespace FastIO
+{
+char buf[1 << 21], buf2[1 << 21], a[20], *p1 = buf, *p2 = buf, hh1 = '\n', hh2 = ' ';
+int p, p3 = -1;
+void read() {}
+void print() { buf2[p3] = hh1; }
+inline int getc()
+{
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1 << 21, stdin), p1 == p2) ? EOF : *p1++;
+}
+inline void flush()
+{
+    fwrite(buf2, 1, p3 + 1, stdout), p3 = -1;
+}
+template <typename T, typename... T2>
+inline void read(T &x, T2 &... oth)
+{
+    int f = 0;
+    x = 0;
+    char ch = getc();
+    while (!isdigit(ch))
+    {
+        if (ch == '-')
+            f = 1;
+        ch = getc();
+    }
+    while (isdigit(ch))
+    {
+        x = x * 10 + ch - 48;
+        ch = getc();
+    }
+    x = f ? -x : x;
+    read(oth...);
+}
+template <typename T, typename... T2>
+inline void print(T x, T2... oth)
+{
+    if (p3 > 1 << 20)
+        flush();
+    if (x < 0)
+        buf2[++p3] = 45, x = -x;
+    do
+    {
+        a[++p] = x % 10 + 48;
+    } while (x /= 10);
+    do
+    {
+        buf2[++p3] = a[p];
+    } while (--p);
+    buf2[++p3] = hh2;
+    print(oth...);
+}
+} // namespace FastIO
+using FastIO::print;
+using FastIO::read;
+//======================================
+#include <cstring>
+const int maxn = 2e3+5;
+const int maxm = 3e3+5;
+inline int add(int a,int b) //∞+x=∞，所以要手写加法函数判断这种情况
+{
+    if(a==0x3f3f3f3f||b==0x3f3f3f3f) return 0x3f3f3f3f;
+    else return a+b;
+}
+struct E
+{
+    int u,v,w;
+}Edge[maxm<<1];
+int dis[maxn];
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+#endif
+    clock_t c1 = clock();
+    //======================================
+    int t;
+    read(t);
+    while(t--)
+    {
+        memset(Edge,0,sizeof(Edge));
+        memset(dis,0x3f,sizeof(dis));
+        dis[1]=0;
+        int n,m,i,k;
+        read(n,m);
+        for(i=k=0;i<m;i++,k++)
+        {
+            int u,v,w;
+            read(u,v,w);
+            Edge[k]=(E){u,v,w};
+            if(w>=0) Edge[++k]=(E){v,u,w};
+        }
+        bool flag = false;      //以下除了手写加法函数以外其它的都与2P相同
+        for(int i=1;i<n;i++)
+        {
+            flag = true;
+            for(int j=0;j<k;j++)
+            {
+                if(add(dis[Edge[j].u],Edge[j].w)<dis[Edge[j].v])
+                {
+                    dis[Edge[j].v]=add(dis[Edge[j].u],Edge[j].w);
+                    flag = false;
+                }
+            }
+            if(flag) break;
+        }
+        for(int j=0;j<k;j++)    //判断负环，PPT里的伪代码
+            if(add(dis[Edge[j].u],Edge[j].w)<dis[Edge[j].v])
+                goto YES;
+        puts("N0");
+        continue;
+        YES:
+        puts("YE5");
+    }
+    //======================================
+    FastIO::flush();
+    std::cerr << "Time:" << clock() - c1 << "ms" << std::endl;
+    return 0;
+}
+/*
+ * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+ * │Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│  ┌┐    ┌┐    ┌┐
+ * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘  └┘    └┘    └┘
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
+ * │~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│ │Num│ / │ * │ - │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
+ * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│ │ 7 │ 8 │ 9 │   │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┤ + │
+ * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │               │ 4 │ 5 │ 6 │   │
+ * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
+ * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │     │ 1 │ 2 │ 3 │   │
+ * ├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤ E││
+ * │ Ctrl│ Win│ Alt│         Space         │ Alt│ Win│Menu│Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │←─┘│
+ * └─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
+ */
