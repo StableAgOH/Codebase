5b856dc73f9ba5e180c48d2314e853350a49f777
StableAgOH <1305707442@qq.com>
2020-10-09 15:55:16 +0800

    使用新版舞蹈链的类进行了修改

--- Code/舞蹈链/P1784 数独.cpp
+++ Code/舞蹈链/P1784 数独.cpp
@@ -9,8 +9,10 @@ class DLX
 {
 private:
     struct Node{ int l,r,u,d,row,col; };
-    vector<Node> dcl;
-    vector<int> s,ans;
+    std::vector<Node> dcl;
+    std::vector<int> s,ans;
+    bool ms;                                        //是否多解
+    void (*process)(std::vector<int>&);             //在得到一个解后对答案进行处理
 
     void remove(int col)
     {
@@ -44,7 +46,10 @@ private:
     {
         int col = dcl[0].r;
         if(!col)
+        {
+            process(ans);                           //得到了一个解
             return true;
+        }
         for(int i=col;i;i=dcl[i].r)
             if(s[i]<s[col]) col = i;
         remove(col);
@@ -53,7 +58,8 @@ private:
             ans.push_back(dcl[i].row);
             for(int j=dcl[i].r;j!=i;j=dcl[j].r)
                 remove(dcl[j].col);
-            if(dance()) return true;
+            bool flag = dance();
+            if(flag&&!ms) return true;              //若只需要一个解则return
             for(int j=dcl[i].r;j!=i;j=dcl[j].r)
                 restore(dcl[j].col);
             ans.pop_back();
@@ -63,28 +69,33 @@ private:
     }
 
 public:
-    DLX(int col)
+    DLX(int col,bool hasMultipleSolutions=false)    //传入列数及是否多解
     {
-        s.resize(col+5);       //动态开出空间
+        ms = hasMultipleSolutions;
+        s.resize(col+5);                            //动态开出空间
         for(int i=0;i<=col;i++)
             dcl.push_back( {i-1,i+1,i,i,0,0} );
         dcl[0].l = col;
         dcl[col].r = 0;
     }
-    void addRow(int r,vector<int>& col)     //为十字链表增加一行,r为行编号,col中存放值为1的列的编号
+    void addRow(int row,std::vector<int>& col)      //为十字链表增加一行,row为行编号,col中存放值为1的列的编号
     {
         int first = dcl.size();
         for(int c : col)
         {
             int tmp = dcl.size();
-            dcl.push_back( {tmp-1,tmp+1,dcl[c].u,c,r,c} );
+            dcl.push_back( {tmp-1,tmp+1,dcl[c].u,c,row,c} );
             dcl[c].u = dcl[dcl[c].u].d = tmp;
             s[c]++;
         }
         dcl[dcl.size()-1].r = first;
         dcl[first].l = dcl.size()-1;
     }
-    auto& solve() { return dance(),ans; }   //若无解则ans必为空
+    bool solve(void (*fun)(std::vector<int>&))      //传入一个用于处理每个解的答案的函数
+    {
+        process = fun;
+        return dance();
+    }
 };
 int sdk[11][11];
 enum Task { Grid,Row,Col,Sub };
@@ -100,6 +111,21 @@ inline auto decode(int n)
     r = n%9+1; n/=9;
     x = n;
     return make_tuple(x,r,c);
+}
+inline void transandprint(vector<int>& v)
+{
+    for(auto i : v)
+    {
+        int x,r,c;
+        tie(x,r,c) = decode(i);
+        sdk[r][c] = x+1;
+    }
+    for(int i=1;i<=9;i++)
+    {
+        for(int j=1;j<=9;j++)
+            cout<<sdk[i][j]<<' ';
+        cout<<endl;
+    }
 }
 int main(int argc, char const *argv[])
 {
@@ -130,19 +156,7 @@ int main(int argc, char const *argv[])
             }
         }
     }
-    auto ans = solver.solve();
-    for(auto i : ans)
-    {
-        int x,r,c;
-        tie(x,r,c) = decode(i);
-        sdk[r][c] = x+1;
-    }
-    for(int i=1;i<=9;i++)
-    {
-        for(int j=1;j<=9;j++)
-            cout<<sdk[i][j]<<' ';
-        cout<<endl;
-    }
+    solver.solve(transandprint);
     //======================================
 end:
     cerr << "Time Used:" << clock() - c1 << "ms" << endl;
